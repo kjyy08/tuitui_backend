e616c0a74629eedcdd6f920c718b2493d92b4fe9
ps200093 <49304325+ps200093@users.noreply.github.com>
2024-08-02 17:09:55 +0900

    Update S3Service.java
    Add Folder Path & Can Upload Duplicated name

--- src/main/java/suftware/tuitui/service/S3Service.java
+++ src/main/java/suftware/tuitui/service/S3Service.java
@@ -1,33 +1,36 @@
 package suftware.tuitui.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class S3Service {
 
-    private final AmazonS3 amazonS3Client;
+    @Autowired
+    private AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    public String uploadFile(MultipartFile file) {
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+    public String uploadFile(MultipartFile file, String folder) {
+        String fileName = folder + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         try {
-            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), null));
+            amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), null));
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException("Error while uploading file to S3", e);
         }
 
-        return amazonS3Client.getUrl(bucketName, fileName).toString();
+        return amazonS3.getUrl(bucketName, fileName).toString();
     }
 }
