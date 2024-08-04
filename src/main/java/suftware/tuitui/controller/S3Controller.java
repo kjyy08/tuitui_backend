ac3fbbccd3d634ebb8034baba5c51108dd29ab2b
ìps200093 <ps200093@gmail.com>
2024-08-04 19:57:02 +0900

    Merge S3 Controllers

--- src/main/java/suftware/tuitui/controller/S3Controller.java
+++ src/main/java/suftware/tuitui/controller/S3Controller.java
@@ -1,23 +1,34 @@
 package suftware.tuitui.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import suftware.tuitui.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/")
+@RequestMapping("api/files")    // 버킷 관련 기본 경로
 public class S3Controller {
     private final S3Service s3Service;
 
-    @PostMapping(value = "images", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file, String folderName) {
-        String fileUrl = s3Service.uploadFile(file, folderName);
-        return ResponseEntity.ok(fileUrl);
+    @PostMapping("/upload/{path}")
+    public ResponseEntity<String> uploadFile(@PathVariable("path") String path, @RequestParam MultipartFile file) {
+        String folder;
+        if (path.equals("image")){
+            folder = "image_image";
+        }
+        else if (path.equals("profile")){
+            folder = "profile_image";
+        }
+        else{
+            return ResponseEntity.badRequest().body("Invalid upload type");
+        }
+        String url = s3Service.uploadFile(file, folder);
+        return ResponseEntity.ok(url);
     }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
