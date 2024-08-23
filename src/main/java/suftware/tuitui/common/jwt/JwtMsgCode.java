ef6afce4411303f93e50136cc7c297e7d5ed80fa
ps200093 <49304325+ps200093@users.noreply.github.com>
2024-08-24 02:38:23 +0900

    Update JwtMsgCode.java

--- src/main/java/suftware/tuitui/common/jwt/JwtMsgCode.java
+++ src/main/java/suftware/tuitui/common/jwt/JwtMsgCode.java
@@ -7,20 +7,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum JwtMsgCode {
-    //  200
-    OK(HttpStatus.OK, "JWT-000", "토큰 검증 성공"),
-    CREATE_OK(HttpStatus.OK, "JWT-000", "토큰 생성 성공"),
+    // 200 OK responses
+    OK(HttpStatus.OK, "JWT-001", "토큰 검증 성공"),
+    CREATE_OK(HttpStatus.OK, "JWT-002", "토큰 생성 성공"),
 
-    //  401
-    INVALID(HttpStatus.UNAUTHORIZED, "JWT-000", "토큰의 구성이 잘못됨"),
-    EXPIRED(HttpStatus.UNAUTHORIZED, "JWT-000", "토큰이 만료됨"),
-    UNSUPPORTED(HttpStatus.UNAUTHORIZED, "JWT-000", "지원하지 않는 토큰"),
+    // 401 Unauthorized responses
+    INVALID(HttpStatus.UNAUTHORIZED, "JWT-003", "토큰의 구성이 잘못됨"),
+    EXPIRED(HttpStatus.UNAUTHORIZED, "JWT-004", "토큰이 만료됨"),
+    UNSUPPORTED(HttpStatus.UNAUTHORIZED, "JWT-005", "지원하지 않는 토큰"),
 
-    //  403
-    FORBIDDEN(HttpStatus.FORBIDDEN, "JWT-000", "인가받지 못한 토큰"),
+    // 403 Forbidden responses
+    FORBIDDEN(HttpStatus.FORBIDDEN, "JWT-006", "인가받지 못한 토큰"),
 
-    //  404
-    EMPTY(HttpStatus.NOT_FOUND, "JWT-000", "토큰이 존재하지 않음");
+    // 404 Not Found responses
+    EMPTY(HttpStatus.NOT_FOUND, "JWT-007", "토큰이 존재하지 않음");
 
     private final HttpStatus status;
     private final String code;
