bdd2cc8d8ee775114e769f388aa3d356c74be89b
김주엽 <kjyy08@naver.com>
2024-09-02 20:47:34 +0900

    24.09.01 카카오 로그인 인증 후 JWT 발급으로 수정

--- src/main/java/suftware/tuitui/TuituiApplication.java
+++ src/main/java/suftware/tuitui/TuituiApplication.java
@@ -1,7 +1,14 @@
 package suftware.tuitui;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TuituiApplication {
@@ -9,5 +16,4 @@ public class TuituiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TuituiApplication.class, args);
 	}
-
 }
