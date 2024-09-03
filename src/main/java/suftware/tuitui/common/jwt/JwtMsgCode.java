0bd60323afdf210b73368d0cc9010d2d55749956
김주엽 <kjyy08@naver.com>
2024-09-04 01:52:43 +0900

    24.09.04 user api 수정

--- src/main/java/suftware/tuitui/common/jwt/JwtMsgCode.java
+++ src/main/java/suftware/tuitui/common/jwt/JwtMsgCode.java
@@ -12,11 +12,11 @@ public enum JwtMsgCode {
 
     //  400 Bad Request
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "JWT-003", "잘못된 토큰 발급 요청"),
+    UNSUPPORTED(HttpStatus.BAD_REQUEST, "JWT-006", "지원하지 않는 토큰"),
+    INVALID(HttpStatus.BAD_REQUEST, "JWT-004", "토큰의 구성이 잘못됨"),
 
     // 401 Unauthorized responses
-    INVALID(HttpStatus.UNAUTHORIZED, "JWT-004", "토큰의 구성이 잘못됨"),
     EXPIRED(HttpStatus.UNAUTHORIZED, "JWT-005", "토큰이 만료됨"),
-    UNSUPPORTED(HttpStatus.UNAUTHORIZED, "JWT-006", "지원하지 않는 토큰"),
 
     // 403 Forbidden responses
     FORBIDDEN(HttpStatus.FORBIDDEN, "JWT-007", "인가받지 못한 토큰"),
