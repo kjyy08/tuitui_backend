fba5a5de1156c254fe584d809baef8ea6629c5c7
김주엽 <kjyy08@naver.com>
2024-09-28 00:34:51 +0900

    24.09.28 관리자 페이지 추가

--- src/main/java/suftware/tuitui/admin/controller/AdminCapsuleController.java
+++ src/main/java/suftware/tuitui/admin/controller/AdminCapsuleController.java
@@ -33,8 +33,8 @@ public class AdminCapsuleController {
 
     @DeleteMapping("capsules/{capsuleId}")
     public ResponseEntity<Void> deleteCapsule(@PathVariable(name = "capsuleId") Integer id){
-        timeCapsuleService.deleteCapsule(id);
         timeCapsuleImageService.deleteCapsuleImage(id);
+        timeCapsuleService.deleteCapsule(id);
 
         return ResponseEntity.ok().build();
     }
